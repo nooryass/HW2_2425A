b3ba67d97db771e212417253b02af29f761b363f
nooryass <nooryassin059@gmail.com>
2024-12-29 00:00:44 +0200

    3.3 - extending the program

--- dry/Pirate.cpp
+++ dry/Pirate.cpp
@@ -2,20 +2,27 @@
 #include "Pirate.h"
 
 
-Pirate::Pirate(const string& name): name(name) {}
+Pirate::Pirate(const string& name,int bounty): name(name) ,bounty(bounty){}
 
 
 void Pirate::setName(const string& name){
     this->name = name;
 }
 
+void Pirate::setBounty(int bounty) {
+    this->bounty = bounty;
+}
+
+int Pirate::getBounty() const {
+    return bounty;
+}
 
 std::string Pirate::getName(){
     return name;
 }
 
 
-std::ostream &operator<<(std::ostream &os, const Pirate &pirate){
-    os << pirate.name;
+std::ostream &operator<<(std::ostream &os, const Pirate &pirate) {
+    os << "Pirate: " << pirate.name << ", Bounty: " << pirate.bounty; // Updated output
     return os;
 }
--- dry/Pirate.h
+++ dry/Pirate.h
@@ -8,9 +8,11 @@ using std::string;
 class Pirate {
 private:
     string name;
+    int bounty;
+
 
 public:
-    Pirate(const string& name);
+    Pirate(const string& name,int bounty);
 
     Pirate() = default;
 
@@ -19,6 +21,8 @@ public:
     void setName(const string& name);
 
     string getName();
+    void setBounty(int bounty);
 
+    int getBounty() const;
     friend std::ostream &operator<<(std::ostream &os, const Pirate &pirate);
 };
