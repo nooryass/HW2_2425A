85d6823b7f029eb846ac79916c9a4f237fd8d559
nooryass <nooryassin059@gmail.com>
2024-12-29 01:39:14 +0200

    implementing Matrix - testing needed

---
+++ HW2/Wet/Matrix.cpp
@@ -0,0 +1,202 @@
+﻿#include "Matrix.h"
+#include "Utilities.h"
+#include <cmath>
+
+Matrix::Matrix() : length(0), width(0), data(nullptr) {}
+
+Matrix::Matrix(int numRows, int numCols) : length(numRows), width(numCols) {
+    if (numRows < 0 || numCols < 0) {
+        exitWithError(MatamErrorType::OutOfBounds);
+    }
+    data = new int[numRows * numCols];
+    for (int elementIndex = 0; elementIndex < numRows * numCols; ++elementIndex) {
+        data[elementIndex] = 0;
+    }
+}
+
+Matrix::Matrix(int numRows, int numCols, int initialValue) : length(numRows), width(numCols) {
+    if (numRows < 0 || numCols < 0) {
+        exitWithError(MatamErrorType::OutOfBounds);
+    }
+    data = new int[numRows * numCols];
+    for (int elementIndex = 0; elementIndex < numRows * numCols; ++elementIndex) {
+        data[elementIndex] = initialValue;
+    }
+}
+
+Matrix::Matrix(const Matrix& sourceMatrix) : length(sourceMatrix.length), width(sourceMatrix.width) {
+    if (sourceMatrix.data) {
+        data = new int[length * width];
+        for (int elementIndex = 0; elementIndex < length * width; ++elementIndex) {
+            data[elementIndex] = sourceMatrix.data[elementIndex];
+        }
+    } else {
+        data = nullptr;
+    }
+}
+
+Matrix::~Matrix() {
+    delete[] data;
+}
+
+Matrix& Matrix::operator=(const Matrix& sourceMatrix) {
+    if (this == &sourceMatrix) {
+        return *this;
+    }
+    int* newData = new int[sourceMatrix.length * sourceMatrix.width];
+    for (int elementIndex = 0; elementIndex < sourceMatrix.length * sourceMatrix.width; ++elementIndex) {
+        newData[elementIndex] = sourceMatrix.data[elementIndex];
+    }
+    delete[] data;
+    data = newData;
+    length = sourceMatrix.length;
+    width = sourceMatrix.width;
+    return *this;
+}
+
+int& Matrix::operator()(int rowIndex, int colIndex) {
+    if (rowIndex < 0 || rowIndex >= length || colIndex < 0 || colIndex >= width) {
+        exitWithError(MatamErrorType::OutOfBounds);
+    }
+    return data[width * rowIndex + colIndex];
+}
+
+const int& Matrix::operator()(int rowIndex, int colIndex) const {
+    if (rowIndex < 0 || rowIndex >= length || colIndex < 0 || colIndex >= width) {
+        exitWithError(MatamErrorType::OutOfBounds);
+    }
+    return data[width * rowIndex + colIndex];
+}
+
+Matrix& Matrix::operator+=(const Matrix& otherMatrix) {
+    if (otherMatrix.length != length || otherMatrix.width != width) {
+        exitWithError(MatamErrorType::UnmatchedSizes);
+    }
+    for (int elementIndex = 0; elementIndex < length * width; ++elementIndex) {
+        data[elementIndex] += otherMatrix.data[elementIndex];
+    }
+    return *this;
+}
+
+Matrix& Matrix::operator-=(const Matrix& otherMatrix) {
+    return *this += -otherMatrix;
+}
+
+Matrix& Matrix::operator*=(const Matrix& otherMatrix) {
+    if (width != otherMatrix.length) {
+        exitWithError(MatamErrorType::UnmatchedSizes);
+    }
+    Matrix resultMatrix(length, otherMatrix.width);
+    for (int rowIndex = 0; rowIndex < length; ++rowIndex) {
+        for (int colIndex = 0; colIndex < otherMatrix.width; ++colIndex) {
+            for (int commonIndex = 0; commonIndex < width; ++commonIndex) {
+                resultMatrix(rowIndex, colIndex) += (*this)(rowIndex, commonIndex) * otherMatrix(commonIndex, colIndex);
+            }
+        }
+    }
+    *this = resultMatrix;
+    return *this;
+}
+
+Matrix& Matrix::operator*=(int scalarValue) {
+    for (int elementIndex = 0; elementIndex < length * width; ++elementIndex) {
+        data[elementIndex] *= scalarValue;
+    }
+    return *this;
+}
+
+Matrix Matrix::operator-() const {
+    return *this * -1;
+}
+
+Matrix Matrix::rotateClockwise() const {
+    Matrix turned_matrix_clockwise(width, length);
+    for (int j = 0; j < width; j++)
+        for (int i = length - 1, a = 0; i >= 0; i--, a++) {
+            turned_matrix_clockwise(j, a) = this->operator()(i, j);
+        }
+    return turned_matrix_clockwise;
+}
+
+Matrix Matrix::rotateCounterClockwise() const {
+    return this->rotateClockwise().rotateClockwise().rotateClockwise();
+}
+
+Matrix Matrix::transpose() const {
+    Matrix transposedMatrix(width, length);
+    for (int rowIndex = 0; rowIndex < length; ++rowIndex) {
+        for (int colIndex = 0; colIndex < width; ++colIndex) {
+            transposedMatrix(colIndex, rowIndex) = (*this)(rowIndex, colIndex);
+        }
+    }
+    return transposedMatrix;
+}
+
+double Matrix::CalcFrobeniusNorm(const Matrix& matrix) {
+    double sumOfSquares = 0.0;
+    for (int rowIndex = 0; rowIndex < matrix.length; ++rowIndex) {
+        for (int colIndex = 0; colIndex < matrix.width; ++colIndex) {
+            sumOfSquares += std::pow(matrix(rowIndex, colIndex), 2);
+        }
+    }
+    return std::sqrt(sumOfSquares);
+}
+
+Matrix operator+(const Matrix& matrix1, const Matrix& matrix2) {
+    return Matrix(matrix1) += matrix2;
+}
+
+Matrix operator-(const Matrix& matrix1, const Matrix& matrix2) {
+    return Matrix(matrix1) -= matrix2;
+}
+
+Matrix operator*(const Matrix& matrix1, const Matrix& matrix2) {
+    return Matrix(matrix1) *= matrix2;
+}
+
+Matrix operator*(const Matrix& matrix, int scalarValue) {
+    return Matrix(matrix) *= scalarValue;
+}
+
+Matrix operator*(int scalarValue, const Matrix& matrix) {
+    return matrix * scalarValue;
+}
+
+bool operator==(const Matrix& matrix1, const Matrix& matrix2) {
+    if (matrix1.length != matrix2.length || matrix1.width != matrix2.width) {
+        return false;
+    }
+    for (int i = 0; i < matrix1.length; ++i) {
+        for (int j = 0; j < matrix1.width; ++j) {
+            if (matrix1(i, j) != matrix2(i, j)) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+bool operator!=(const Matrix& matrix1, const Matrix& matrix2) {
+    return !(matrix1 == matrix2);
+}
+
+Matrix turnToScalarMatrix(int scalarValue, int numRows, int numCols) {
+    Matrix scalarMatrix(numRows, numCols);
+    for (int diagonalIndex = 0; diagonalIndex < numRows; ++diagonalIndex) {
+        scalarMatrix(diagonalIndex, diagonalIndex) = scalarValue;
+    }
+    return scalarMatrix;
+}
+
+std::ostream& operator<<(std::ostream& outputStream, const Matrix& matrix) {
+    if (matrix.length == 0 || matrix.width == 0) {
+        return outputStream;
+    }
+    for (int rowIndex = 0; rowIndex < matrix.length; ++rowIndex) {
+        for (int colIndex = 0; colIndex < matrix.width; ++colIndex) {
+            outputStream << "|" << matrix(rowIndex, colIndex);
+        }
+        outputStream << "|" << std::endl;
+    }
+    return outputStream;
+}
---
+++ HW2/Wet/Matrix.h
@@ -0,0 +1,103 @@
+﻿//
+// Created by Nataly Haddad on 09/07/2024.
+//
+
+#ifndef WET_Matrix_H
+#define WET_Matrix_H
+
+#include <iostream>
+#include "Utilities.h"
+
+class Matrix {
+
+private:
+
+    int length; // number of rows
+    int width; // number of columns
+    int* data; // one-dimensional array to store Matrix elements
+
+public:
+    
+    // constructor that initializes a matrix with initValue
+    Matrix(int n, int m, int initialValue); 
+
+    // constructor that initializes a matrix with zeroes
+    Matrix( int n, int m );
+
+    // default constructor that initializes a 0x0 Matrix
+    Matrix();
+
+    // copy constructor
+    Matrix(const Matrix& matrix);
+
+    // assignment operator
+    Matrix& operator=(const Matrix& matrix);
+
+    // destructor
+    ~Matrix();
+
+    // operator () for accessing elements in the matrix (non-const version)
+    int& operator()(int row, int column);
+
+    // operator () for accessing elements in the matrix (const version)
+    const int& operator()(int row, int column) const;
+
+    // addition assignment operator
+    Matrix& operator+=(const Matrix& matrix);
+
+    // subtractions assignment operator
+    Matrix& operator-=(const Matrix& matrix);
+
+    // multiplication assignment operator
+    Matrix& operator*=(const Matrix& matrix);
+
+    // unary minus operator
+    Matrix operator-() const;
+
+    // multiplication by scalar assignment operator
+    Matrix& operator*=(const int scalar);
+
+    // returns rotated matrix clockwise
+    Matrix rotateClockwise() const;
+
+    // returns rotated matrix counterclockwise
+    Matrix rotateCounterClockwise() const;
+
+    // returns transposed matrix
+    Matrix transpose() const;
+
+    // prints matrix
+    friend std::ostream& operator<<(std::ostream& os, const Matrix& Matrix);
+
+    // equality operator
+    friend bool operator==(const Matrix& matrix1, const Matrix& matrix2);
+
+    static double CalcFrobeniusNorm(const Matrix& matrix);
+
+};
+
+// matrix addition operator
+Matrix operator+(const Matrix& matrix1, const Matrix& matrix2);
+
+// matrix subtraction operator
+Matrix operator-(const Matrix& matrix1, const Matrix& matrix2);
+
+// matrix multiplication operator
+Matrix operator*(const Matrix& matrix1, const Matrix& matrix2);
+
+// multiplication by scalar operator
+Matrix operator*(const int scalar, const Matrix& matrix);
+
+// multiplication by scalar operator
+Matrix operator*(const Matrix& matrix, const int scalar);
+
+// inequality operator
+bool operator!=(const Matrix& matrix1, const Matrix& matrix2);
+
+// turns a scalar to a scalar matrix
+Matrix turnToScalarMatrix(int scalar, int length, int width);
+
+// Method to calculate Frobenius norm of the current matrix
+
+
+#endif //WET_Matrix_H
